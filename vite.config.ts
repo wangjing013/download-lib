41e26843cb3ccfcbe729d29d777007434f64cbc8
msb_39367 <wangjing19920528@163.com>
2026-01-23 18:53:10 +0800

    feat: 添加 vue 解析插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
     outDir: 'lib',
   },
   plugins: [
+    vue(),
     dts({
       rollupTypes: true,
       tsconfigPath: './tsconfig.app.json',
